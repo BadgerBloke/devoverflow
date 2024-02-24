dc657bce6995faed140b1d4b391c1a97562db7f4
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2024-02-24 20:53:07 +0530

    left sidebar added

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 
+import LeftSideBar from "~/components/shared/left-sidebar";
 import Navbar from "~/components/shared/navbar";
 
 const Layout = ({ children }: { children: ReactNode }) => {
@@ -7,7 +8,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSideBar
+        <LeftSideBar />
         <section className="flex min-h-svh flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
