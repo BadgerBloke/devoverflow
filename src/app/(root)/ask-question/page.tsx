e5f25feac02ea9ec38e709fa20291a75007820fb
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2024-02-25 02:12:38 +0530

    question form component implemented

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,5 +1,14 @@
+import Question from "~/components/forms/question";
+
 const Page = () => {
-  return <div>Ask Question</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
