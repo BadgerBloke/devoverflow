38bd56e485758984418c01d0dadcd94a25f4d189
Mukesh Singh <camukeshkumarsingh@gmail.com>
2024-03-15 10:09:02 +0530

    tags implemented

--- src/components/forms/question.tsx
+++ src/components/forms/question.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useRef } from "react";
-import { useForm } from "react-hook-form";
+import { KeyboardEvent, useRef } from "react";
+import Image from "next/image";
+import { ControllerRenderProps, useForm } from "react-hook-form";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
@@ -19,6 +20,8 @@ import {
 import { Input } from "~/components/ui/input";
 import { QuestionSchema, QuestionType } from "~/lib/validations";
 
+import { Badge } from "../ui/badge";
+
 const Question = () => {
   const editorRef = useRef(null);
   const form = useForm<QuestionType>({
@@ -29,6 +32,49 @@ const Question = () => {
       tags: [],
     },
   });
+
+  const handleInputKeyDown = (
+    e: KeyboardEvent<HTMLInputElement>,
+    field: ControllerRenderProps<
+      {
+        title: string;
+        explanation: string;
+        tags: string[];
+      },
+      "tags"
+    >
+  ) => {
+    console.log("Pressed Key: ", e.key);
+    if (e.key.toLowerCase() === "enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value;
+      console.log("Tags value: ", tagValue);
+      try {
+        form.setValue("tags", [...field.value, tagValue]);
+        tagInput.value = "";
+        QuestionSchema.pick({ tags: true }).parse([...field.value, tagValue]);
+      } catch (err) {
+        // form.trigger("tags");
+      }
+    }
+  };
+
+  const handleTagRemove = (
+    tag: string,
+    field: ControllerRenderProps<
+      {
+        title: string;
+        explanation: string;
+        tags: string[];
+      },
+      "tags"
+    >
+  ) => {
+    const newTags = field.value.filter((t) => t !== tag);
+    form.setValue("tags", newTags);
+  };
 
   const onSubmit = () => {};
   return (
@@ -123,13 +169,32 @@ const Question = () => {
                 Tags <span className="mt-3.5 text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                {/* TODO: Add an Editor component */}
                 <Input
                   placeholder="Add tags..."
                   className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-14 border"
-                  {...field}
+                  onKeyDown={(e) => handleInputKeyDown(e, field)}
                 />
               </FormControl>
+              {field.value.length > 0 && (
+                <div className="flex-start mt-2.5 gap-2.5">
+                  {field.value.map((tag) => (
+                    <Badge
+                      className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                      key={tag}
+                      onClick={() => handleTagRemove(tag, field)}
+                    >
+                      {tag}
+                      <Image
+                        src="/assets/icons/close.svg"
+                        alt="Close icon"
+                        width={12}
+                        height={12}
+                        className="cursor-pointer object-contain invert-0 dark:invert"
+                      />
+                    </Badge>
+                  ))}
+                </div>
+              )}
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 tags to describe what your question is about. You
                 need to press enter to add a tag.
--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -3,7 +3,13 @@ import z from "zod";
 export const QuestionSchema = z.object({
   title: z.string().min(5).max(130),
   explanation: z.string().min(100),
-  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+  tags: z
+    .array(z.string().min(1).max(15))
+    .min(1)
+    .max(3)
+    .refine((values) => new Set(values).size === values.length, {
+      message: "Tags must be unique",
+    }),
 });
 
 export type QuestionType = z.infer<typeof QuestionSchema>;
