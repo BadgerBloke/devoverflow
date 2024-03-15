2ca3ef2acc0b48ea90d511cba814e7945d46c9a2
Mukesh Singh <camukeshkumarsingh@gmail.com>
2024-03-15 06:29:56 +0530

    sign-in and sign-up layout fixed

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 const Page = () => {
-  return <SignUp />;
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center">
+      <SignUp />
+    </div>
+  );
 };
 
 export default Page;
