878c6c2186a13a8a1a22384b37ed20a37b38b675
Mukesh Singh <camukeshkumarsingh@gmail.com>
2024-03-17 17:55:32 +0530

    title changed

--- src/components/shared/navbar/index.tsx
+++ src/components/shared/navbar/index.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           src="/assets/images/site-logo.svg"
           width={23}
           height={23}
-          alt="DevOverflow"
+          alt="CivilOverflow"
         />
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Civil<span className="text-primary-500">Overflow</span>
