3cecc443097107e78c169f9845615ae8c72bb64b
Mukesh Singh <camukeshkumarsingh@gmail.com>
2024-03-19 23:59:19 +0530

    ai_answer implemented

--- src/components/forms/answer.tsx
+++ src/components/forms/answer.tsx
@@ -31,6 +31,7 @@ const AnswerForm = ({ question, questionId, authorId }: Props) => {
   const { mode } = useTheme();
   const pathname = usePathname();
   const [isSubmitting, setSubmitting] = useState(false);
+  const [isSubmittingAi, setSubmittingAi] = useState(true);
   const form = useForm<AnswerType>({
     resolver: zodResolver(AnswerSchema),
     defaultValues: {
@@ -59,6 +60,40 @@ const AnswerForm = ({ question, questionId, authorId }: Props) => {
       setSubmitting(false);
     }
   };
+
+  const generateAiAnswer = async () => {
+    if (!authorId) return;
+
+    setSubmittingAi(true);
+
+    try {
+      // const res = await fetch(
+      //   `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+      //   {
+      //     method: "POST",
+      //     body: JSON.stringify({ question }),
+      //   }
+      // );
+      const res = {
+        reply:
+          "In Rust, `String` and `str` are two distinct data types that are related to representing strings but have different characteristics.\n\n1. `String` is a growable, heap-allocated, UTF-8 encoded string data type provided by Rust's standard library. It is mutable and can be modified easily. It is owned and allows for dynamic manipulation and resizing of the underlying string data. `String` is typically used when you need to work with owned, mutable strings that can grow or shrink at runtime.\n\n2. `str`, on the other hand, is an immutable view into a sequence of UTF-8 bytes, which is often referred to as a string slice. It is a reference to a sequence of bytes stored elsewhere in memory. Immutable means that you cannot modify the content of a `str` directly. It is a borrowed type and primarily used for string literals, function parameters, or as a slice of a `String`. String slices are commonly used as a more efficient and ergonomic way to work with string data without incurring the overhead of owning the data.\n\nWhen deciding between `String` and `str`, consider the following guidelines:\n\n- Use `String` when you need a mutable, growable string that you own and can modify.\n- Use `str` when you need to reference a part of a `String` or work with string literals and immutable string data.\n\nNeither `String` nor `str` is getting deprecated in Rust. They serve different purposes and are both widely used in Rust programming. It's important to choose the appropriate type based on your specific use case and requirements.",
+      };
+
+      // const aiAnswer = await res.json();
+      const aiAnswer = res;
+
+      const formattedAnswer = aiAnswer.reply.replace(/\n/g, "<br />");
+
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent(formattedAnswer);
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    } finally {
+      setSubmittingAi(false);
+    }
+  };
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -67,16 +102,16 @@ const AnswerForm = ({ question, questionId, authorId }: Props) => {
         </h4>
         <Button
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-          onClick={() => {}}
+          onClick={generateAiAnswer}
         >
           <Image
             src="/assets/icons/stars.svg"
             height={12}
             width={12}
             alt="stars"
-            className="object-contain"
+            className={`object-contain ${isSubmittingAi ? "animate-pulse" : ""}`}
           />
-          Generate an AI Answer
+          {isSubmittingAi ? "Generating..." : "Generate AI Answer"}
         </Button>
       </div>
       <Form {...form}>
