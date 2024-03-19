7c2cf064303ec29d0bd070f4eb3779269f522603
Mukesh Singh <camukeshkumarsingh@gmail.com>
2024-03-19 22:33:08 +0530

    reputation implemented

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -270,7 +270,7 @@ export const getUserQuestions = async (params: GetUserStatsParams) => {
 
     const totalQuestions = await Question.countDocuments({ author: userId });
     const userQuestions = await Question.find({ author: userId })
-      .sort({ views: -1, upVotes: -1 })
+      .sort({ createdAt: -1, views: -1, upVotes: -1 })
       .skip(skipAmount)
       .limit(pageSize)
       .populate("tags", "_id name")
